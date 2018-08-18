2bd9edf538dd0ac5f0204827f10da0257dfbd402
lukasz <lblaszkowski@onet.pl>
2018-08-18 23:53:59 +0200

    doing the task 5

--- fixture/fikstury_application.py
+++ fixture/fikstury_application.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.support.select import Select
 from fixture.session import SessionHelper
+from fixture.manager import ManagerHelper
 
 
 class Fikstury_application:
@@ -9,55 +9,11 @@ class Fikstury_application:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.manager = ManagerHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/edit.php")
-
-    def user_data(self, auxiliary_contact_settings):
-        wd = self.wd
-        wd.find_element_by_name("firstname").send_keys(auxiliary_contact_settings.firstname)
-        wd.find_element_by_name("middlename").send_keys(auxiliary_contact_settings.middlename)
-        wd.find_element_by_name("lastname").send_keys(auxiliary_contact_settings.lastname)
-        wd.find_element_by_name("nickname").send_keys(auxiliary_contact_settings.nickname)
-
-    def adding_a_picture_and_name(self, auxiliary_contact_settings):
-        wd = self.wd
-        wd.find_element_by_name("photo").send_keys("C:\\python_training\\image\\images.jpg")
-        wd.find_element_by_name("title").send_keys(auxiliary_contact_settings.title_photo)
-
-
-    def companys_data(self, company_data_settings):
-        wd = self.wd
-        wd.find_element_by_name("company").send_keys(company_data_settings.company)
-        wd.find_element_by_name("address").send_keys(company_data_settings.address)
-        wd.find_element_by_name("home").send_keys(company_data_settings.home)
-        wd.find_element_by_name("mobile").send_keys(company_data_settings.mobile)
-        wd.find_element_by_name("work").send_keys(company_data_settings.work)
-        wd.find_element_by_name("fax").send_keys(company_data_settings.fax)
-
-    def e_mail_data(self, auxiliary_contact_settings):
-        wd = self.wd
-        wd.find_element_by_name("email").send_keys(auxiliary_contact_settings.email)
-        wd.find_element_by_name("email2").send_keys(auxiliary_contact_settings.email2)
-        wd.find_element_by_name("email3").send_keys(auxiliary_contact_settings.email3)
-
-    def additional_data(self, additional_data_settings):
-        wd = self.wd
-        wd.find_element_by_name("homepage").send_keys(additional_data_settings.homepage)
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(additional_data_settings.bday)
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(additional_data_settings.bmonth)
-        wd.find_element_by_name("byear").send_keys(additional_data_settings.byear)
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(additional_data_settings.aday)
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(additional_data_settings.amonth)
-        wd.find_element_by_name("ayear").send_keys(additional_data_settings.ayear)
-        wd.find_element_by_name("address2").send_keys(additional_data_settings.address2)
-        wd.find_element_by_name("phone2").send_keys(additional_data_settings.phone2)
-        wd.find_element_by_name("notes").send_keys(additional_data_settings.notes)
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact_in_book_address.py
+++ test/test_add_contact_in_book_address.py
@@ -12,13 +12,13 @@ def app(request):
 
 def test_add_contact_in_book_address(app):
     app.session.login( username="admin", password="secret")
-    app.user_data(User_basic_data(firstname="Lukasz", middlename="Ebi", lastname="Blaszkowski", nickname="lblaszkowski"))
-    app.adding_a_picture_and_name( User_picture(title_photo="Chomik"))
-    app.companys_data(Company_data_settings(company="brak nazwy", address="Gdansk", home="Polska",
+    app.manager.user_data(User_basic_data(firstname="Lukasz", middlename="Ebi", lastname="Blaszkowski", nickname="lblaszkowski"))
+    app.manager.adding_a_picture_and_name( User_picture(title_photo="Chomik"))
+    app.manager.companys_data(Company_data_settings(company="brak nazwy", address="Gdansk", home="Polska",
                                                      mobile="123456789",work="tester", fax="123456789"))
-    app.e_mail_data( Data_email(email="janekkolasa@wp.pl", email2="janekkolasa2@wp.pl", email3="janekkolasa3@wp.pl"))
-    app.additional_data( Additional_data_settings(homepage="brak",bday="2", bmonth="October",byear="1990",
+    app.manager.e_mail_data( Data_email(email="janekkolasa@wp.pl", email2="janekkolasa2@wp.pl", email3="janekkolasa3@wp.pl"))
+    app.manager.additional_data( Additional_data_settings(homepage="brak",bday="2", bmonth="October",byear="1990",
                                 aday="3",amonth="February",ayear="2001",address2="brak", phone2="brak", notes="brak"))
-    app.return_to_groups_page()
+    app.manager.return_to_groups_page()
     app.session.logout()
 
