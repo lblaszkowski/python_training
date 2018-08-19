2b0ad12d107860f0feceba04ca1f4b24ed7f77af
lukasz <lblaszkowski@onet.pl>
2018-08-19 12:36:03 +0200

    refactoring code

--- fixture/group.py
+++ fixture/group.py
@@ -7,21 +7,27 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def init_creation(self):
+    def create_group(self, settings_ancillary):
         wd = self.app.wd
         self.open_groups_page()
+        # init group creation
         wd.find_element_by_name("new").click()
-
-    def fill_creation(self, settings_ancillary):
-        wd = self.app.wd
+        # fill  group form
         wd.find_element_by_id("content").click()
         wd.find_element_by_name("group_name").send_keys(settings_ancillary.name)
         wd.find_element_by_name("group_header").send_keys(settings_ancillary.header)
         wd.find_element_by_name("group_footer").send_keys(settings_ancillary.footer)
-
-    def submit_creation(self):
-        wd = self.app.wd
+        # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,25 +1,14 @@
-import pytest
-from fixture.application import Application
 from model.group import Group
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_group(app):
     app.session.login( username="admin", password="secret")
-    app.group.init_creation()
-    app.group.fill_creation(Group(name="test", header="test", footer="test"))
-    app.group.submit_creation()
+    app.group.create_group(Group(name="test", header="test", footer="test"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.init_creation()
-    app.group.fill_creation(Group(name="", header="", footer=""))
-    app.group.submit_creation()
+    app.group.create_group(Group(name="", header="", footer=""))
     app.session.logout()
 
