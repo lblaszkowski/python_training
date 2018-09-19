4e7078557e7f981305cef3747b3cab1daec6924b
lukasz <lblaszkowski@onet.pl>
2018-09-19 22:06:40 +0200

    the task 9  does not work correctly

--- test/test_del_contact_in_book_address.py
+++ test/test_del_contact_in_book_address.py
@@ -3,5 +3,4 @@ from model.contact import Contact
 def test_del_contact_in_book_address(app):
     app.session.edit_page_settings()
     if app.group.count() == 0:
-        app.group.fill_all_user_data(Contact(firstname="Test"))
-    app.contact.del_contact_in_book_address()
+        app.contact.del_contact_in_book_address()
